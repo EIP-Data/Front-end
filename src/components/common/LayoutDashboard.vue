fa73eae7cacdb95ae9dadc5c106e4110f449448c
cometerlecki <cometerlecki@gmail.com>
2025-10-08 20:24:38 +1100

    feat: layout

---
+++ src/components/common/LayoutDashboard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex h-screen bg-gray-100">
+    <Sidebar />
+    <div class="flex flex-col flex-1">
+      <Navbar />
+      <main class="p-6 overflow-auto flex-1">
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
